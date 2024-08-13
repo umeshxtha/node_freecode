4577043b4c6e5fac4afdbb6b2c40beedd21ea827
umeshxtha <umesh.xtha123@gmail.com>
2024-08-13 22:39:27 +0545

    static path from public server doesnot need changes too

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
-const app = express()
+const path = require('path')
+const app = express();
+
+// setup static and middleware
+
+app.use(express.static('./public'))
 app.get('/',(req,res)=>{
-res.send('homepage')
-console.log()
+res.sendFile(path.resolve(__dirname,'./navbar-app/index.html')
+)
 })
 
-// app.get 
-app.listen(5000,()=>{
-    console.log('serve is listening')
+app.all('*',(res,req)=>{
+    res.status(404).send('404 Not Found')
 })
+
+app.listen(5000,(req,res)=>{
+    console.log('server is running on port 5000')
+})
+
